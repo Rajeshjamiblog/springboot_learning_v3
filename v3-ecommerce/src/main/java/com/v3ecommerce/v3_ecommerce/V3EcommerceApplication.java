f48ed756d63b2d17425cda80c42efe4e1dcd6019
Rajeshjamiblog <rajeshjamiblog@gmail.com>
2025-11-05 17:47:00 +0530

    initial v3 commerce project

---
+++ v3-ecommerce/src/main/java/com/v3ecommerce/v3_ecommerce/V3EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.v3ecommerce.v3_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V3EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V3EcommerceApplication.class, args);
+	}
+
+}
---
+++ v3-ecommerce/src/test/java/com/v3ecommerce/v3_ecommerce/V3EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v3ecommerce.v3_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V3EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
